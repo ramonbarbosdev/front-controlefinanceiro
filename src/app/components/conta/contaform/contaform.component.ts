8b841c81a18f2547b8bdcefe860b6e878b8eb4c1
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-05-05 17:39:22 -0300

    feat: implementa formulário de cadastro de conta com validação e feedback de sucesso/erro

--- src/app/components/conta/contaform/contaform.component.ts
+++ src/app/components/conta/contaform/contaform.component.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InputTextComponent } from '../../component/input-text/input-text.component';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../component/button/button.component';
+import { Conta } from '../../../models/conta';
+import { ContaService } from '../../../services/conta.service';
+import Swal from 'sweetalert2';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-contaform',
-  imports: [],
+  imports: [InputTextComponent, RouterModule, ButtonComponent],
   templateUrl: './contaform.component.html',
-  styleUrl: './contaform.component.scss'
+  styleUrl: './contaform.component.scss',
 })
 export class ContaformComponent {
+  public objeto: Conta[] | any = [];
 
+  service = inject(ContaService);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  location = inject(Location);
+  nm_titulo = 'Cadastrar Conta';
+
+    ngOnInit() {
+      const key = this.route.snapshot.paramMap.get('id');
+      this.onEdit(key);
+      if (!key) this.obterSequencia();
+    }
+
+    onClose() {
+      this.location.back();
+    }
+
+    onEdit(id: any) {
+      this.service.obterPorId(id).subscribe({
+        next: (res: any) => {
+          this.objeto = res;
+        },
+        error: (err) => {
+          Swal.fire({
+            icon: 'error',
+            title: err.error.code,
+            text: err.error.error,
+            confirmButtonText: 'OK',
+          });
+        },
+      });
+    }
+
+    obterSequencia() {
+      this.service.sequencia().subscribe({
+        next: (res: any) => {
+          this.objeto.cd_conta = res;
+        },
+        error: (err) => {
+          Swal.fire({
+            icon: 'error',
+            title: err.error.code,
+            text: err.error.error,
+            confirmButtonText: 'OK',
+          });
+        },
+      });
+    }
+
+    onSave() {
+      this.service.cadastrar(this.objeto).subscribe({
+        next: (res: any) => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso',
+            text: 'Tipo de conta cadastrado com sucesso!',
+            confirmButtonText: 'OK',
+          });
+          this.onClose();
+        },
+        error: (err) => {
+          Swal.fire({
+            icon: 'error',
+            title: err.error.code,
+            text: err.error.error,
+            confirmButtonText: 'OK',
+          });
+        },
+      });
+    }
 }
